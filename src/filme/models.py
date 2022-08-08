fbf424ab47c735ce2496acd673487d5ba54ff7a4
Juan Felipe <103801874+JuanLadeira@users.noreply.github.com>
2022-08-07 22:07:47 -0300

    models.py Filmes
    Criação do models do app filmes

--- src/filme/models.py
+++ src/filme/models.py
@@ -1,3 +1,18 @@
+from distutils.command.upload import upload
 from django.db import models
 
+LISTA_CATEGORIAS = (
+    ("ANALISES", 'Análises'),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros")
+    )
+
 # Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to="thumb_filmes")
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(auto_now_add=True)
