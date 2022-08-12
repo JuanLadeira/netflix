2029aafe5680907156208ab8410170e2a87c160d
juan <juan.fll@hotmail.com>
2022-08-12 17:16:50 -0300

    aa

--- src/filme/urls.py
+++ src/filme/urls.py
@@ -3,5 +3,5 @@ from .views import  HomePage , HomeFilme, DetalhesFilme
 urlpatterns = [
     path('', HomePage.as_view() , name='home-filme'),
     path('filmes/', HomeFilme.as_view() , name='home-page'),
-    path('filmes/<pk>/', DetalhesFilme.as_view() , name='detalhe-filme'),
+    path('filmes/<int:pk>/', DetalhesFilme.as_view() , name='detalhe-filme'),
 ]
